f3fc338c75a1db6c0fd52ecdf2919742719bb067
Alexandr Dem'yanenko <demjanenko@gmail.com>
2016-01-17 01:13:06 +0300

    c# build cmdline

--- build-cs.sh
+++ build-cs.sh
@@ -1,1 +1,1 @@
-haxe -main MainCs.hx -cp aggx -cp examples/source -cp csharp -cs export-csharp -v
+haxe -main MainCs.hx -cp aggx -cp examples/source -cp csharp -cs export-csharp -D dll -D erase-generics -v
